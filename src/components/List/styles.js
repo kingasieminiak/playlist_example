3529ac4eb3905a52075cabff265320fca7a584e0
Kinga Sieminiak <kinga.sieminiak@elpassion.pl>
2019-11-27 20:00:16 +0100

    Remove scroll list

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,17 +1,9 @@
 import styled from 'styled-components';
-import { mediaMax, breakpoints } from '../../styles/mediaQueries';
 
 export const List = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
-
-  ${mediaMax(breakpoints.md)} {
-    height: 350px;
-    overflow-y: scroll;
-    padding: .2rem;
-    padding-bottom: 1rem;
-  }
 `;
 
 List.Item = styled.li`
